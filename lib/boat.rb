1e223c4269418e9cc2dc64ea9da882e3b0584804
Rene Casco <rene.casco@icloud.com>
2018-09-05 13:40:43 -0600

    Iteration 1 completed

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,7 +1,7 @@
 
 class Boat
   attr_reader :type,
-              :price_per_hour
+              :price_per_hour,
               :hours_rented
 
   def initialize(type, price_per_hour)
@@ -10,6 +10,9 @@ class Boat
     @hours_rented = 0
   end
 
+  def add_hour
+    @hours_rented += 1
+  end
 
 
 end
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -20,9 +20,29 @@ class BoatTest < Minitest::Test
     assert_equal 20, kayak.price_per_hour
   end
 
-  def test_hours_rented_is_initialized_to_zero
+  def test_it_has_hours_rented
     kayak = Boat.new(:kayak, 20)
     assert_equal 0, kayak.hours_rented
   end
 
+  def test_it_adds_second_hour
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    assert_equal 2, kayak.add_hour
+  end
+
+  def test_it_adds_third_hour
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+    assert_equal 3, kayak.add_hour
+  end
+
+  def test_hours_rented_has_total_hours
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+    assert_equal 3, kayak.hours_rented
+  end
 end
